c6c484ac48dc2ca8f3c631d7f34f8942adedf06d
Administrator <administrator@milenas-iMac.local>
2021-08-08 18:37:50 +0930

    Desafio 02

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,37 @@
-export function SideBar() {
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface SideBarProps {
+  selectedGenreId: number;
+  genres: GenreResponseProps[];
+  handleClickButton: (id: number) => void;
+}
+
+import { Button } from "./Button"
+
+export function SideBar({selectedGenreId, genres, handleClickButton}: SideBarProps) {
   // Complete aqui
+
+  return(
+    
+      <nav className="sidebar">
+        <span>Watch<p>Me</p></span>
+
+        <div className="buttons-container">
+          {genres.map(genre => (
+            <Button
+              key={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => handleClickButton(genre.id)}
+              selected={selectedGenreId === genre.id}
+            />
+          ))}
+        </div>
+
+      </nav>
+  );
 }
